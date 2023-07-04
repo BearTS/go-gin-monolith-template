0c236125492820636b1536e2e7cb97fb7455d83b
Anuj Parihar <anujparihar@yahoo.com>
2023-07-04 14:50:13 +0530

    chore: use of vanilla cors configuration

--- app/routes.go
+++ app/routes.go
@@ -1,31 +1,20 @@
 package app
 
 import (
-	"time"
-
+	"github.com/BearTS/go-gin-monolith/app/middleware"
 	"github.com/BearTS/go-gin-monolith/controllers/v1/user"
 	"github.com/BearTS/go-gin-monolith/database"
 	"github.com/BearTS/go-gin-monolith/dbops/gorm/otp_verifications"
 	"github.com/BearTS/go-gin-monolith/dbops/gorm/users"
 	"github.com/BearTS/go-gin-monolith/services/authsvc"
 	"github.com/BearTS/go-gin-monolith/services/usersvc"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func MapURL() {
 	router := gin.Default()
 
-	router.Use(cors.New(cors.Config{
-		// AllowOrigins:     []string{"*", "http://localhost:3000", "https://localhost:3000", "http://localhost:5000"},
-		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
-		AllowAllOrigins:  true,
-		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
-		AllowCredentials: true,
-		AllowWebSockets:  true,
-		MaxAge:           12 * time.Hour,
-	}))
+	router.Use(middleware.CORSMiddleware())
 
 	gormDB, _ := database.Connection()
 	usersGorm := users.Gorm(gormDB)
